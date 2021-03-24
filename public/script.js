d51e2cc420301fb9401f3f26b311d6177e902c73
dbongiov <61100159+dbongiov@users.noreply.github.com>
2021-03-23 20:43:14 -0400

    table?

---
+++ public/script.js
@@ -0,0 +1,24 @@
+app.route('/api')
+  .get(async (req, res) => {
+    console.log('GET request detected');
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+    console.log('data from fetch', json[0]);
+    res.json(json);
+  })
+  .post(async (req, res) => {
+    console.log('POST request detected');
+    console.log('Form data in res.body', req.body);
+    console.log('Now send something back to your client');
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+
+    fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
+      .then((data) => data.json())
+      .then((data2) => {
+        // do something with your data!
+      })
+      .catch((err) => console.error(err));
+
+    res.json({data: json});
+  });
