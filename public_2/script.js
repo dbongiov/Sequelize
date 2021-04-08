e521a4c39c68ef14d34762fd5a7e3eab8bf88b19
dbongiov <61100159+dbongiov@users.noreply.github.com>
2021-04-08 18:46:32 -0400

    updated

--- public_2/script.js
+++ public_2/script.js
@@ -1,21 +1,28 @@
+// const tableBody = document.querySelector('.tableBody');
 
-const tableBody = document.querySelector('.tableBody');
-async function getDiningHalls() {
-  const hallRequest = await fetch('/api/dining');
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+async function getMeals() {
+  console.log('data request');
+  const hallRequest = await fetch('/api/wholeMeal');
   const hallData = await hallRequest.json();
-
-  hallData.data.forEach((hall) => {
-    const appendLine = document.createElement('tr');
-    appendLine.innerHTML = `
-        <td>${hall.hall_id}</td>
-        <td>${hall.hall_name}</td>
-        <td>${hall.hall_address}</td>`;
-    tableBody.append(appendLine);
-  });
+  return hallData;
 }
 
 async function windowActions() {
-  getDiningHalls();
+  console.log('loaded window');
+  const results = await getMeals();
+  const meals = results.data;
+  const mealArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const selectedMeals = mealArray.map(element => {
+    const random = getRandomIntInclusive(0, meals.data.length - 1);
+    return meals.data[random];
+  });
+  console.table(selectedMeals);
 }
 
 window.onload = windowActions;
